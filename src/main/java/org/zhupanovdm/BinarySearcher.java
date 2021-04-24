9e4363db8d337bcdab83f3136d34585817014a52
Zhupanov Dmitry <zhupanov.dmitry@gmail.com>
2021-04-24 21:04:15 +0300

    Initial commit

---
+++ src/main/java/org/zhupanovdm/BinarySearcher.java
@@ -0,0 +1,52 @@
+package org.zhupanovdm;
+
+public class BinarySearcher<T extends Comparable<T>> {
+    private final T[] data;
+
+    public BinarySearcher(T[] data) {
+        this.data = data;
+    }
+
+    public int search(T value) {
+        int low = 0;
+        int high = data.length - 1;
+        while (low <= high) {
+            int i = low + (high - low) / 2;
+            T v = data[i];
+
+            int result = value.compareTo(v);
+            if (result == 0) {
+                return i;
+            }
+
+            if (result > 0) {
+                low = i + 1;
+            } else {
+                high = i - 1;
+            }
+        }
+        return -1;
+    }
+
+    public int[] searchBounds(T value) {
+        int found = search(value);
+        if (found < 0)
+            return null;
+
+        int[] bounds = new int[2];
+        boolean goOn = true;
+        for (int i = 0; goOn; i++) {
+            goOn = false;
+            if (found - i >= 0 && data[found - i].equals(value)) {
+                bounds[0] = found - i;
+                goOn = true;
+            }
+            if (found + i < data.length && data[found + i].equals(value)) {
+                bounds[1] = found + i;
+                goOn = true;
+            }
+        }
+        return bounds;
+    }
+
+}
---
+++ src/test/java/org/zhupanovdm/BinarySearcherTest.java
@@ -0,0 +1,78 @@
+package org.zhupanovdm;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BinarySearcherTest {
+
+    @Test
+    public void testEven() {
+        BinarySearcher<Integer> searcher = new BinarySearcher<>(new Integer[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 });
+        assertEquals(-1, searcher.search(-1));
+        assertEquals(0, searcher.search(0));
+        assertEquals(4, searcher.search(4));
+        assertEquals(5, searcher.search(5));
+        assertEquals(6, searcher.search(6));
+        assertEquals(9, searcher.search(9));
+        assertEquals(-1, searcher.search(10));
+    }
+
+    @Test
+    public void testNonEven() {
+        BinarySearcher<Integer> searcher = new BinarySearcher<>(new Integer[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 });
+        assertEquals(-1, searcher.search(-1));
+        assertEquals(0, searcher.search(0));
+        assertEquals(4, searcher.search(4));
+        assertEquals(5, searcher.search(5));
+        assertEquals(6, searcher.search(6));
+        assertEquals(7, searcher.search(7));
+        assertEquals(10, searcher.search(10));
+        assertEquals(-1, searcher.search(11));
+    }
+
+    @Test
+    public void testEmpty() {
+        BinarySearcher<Integer> searcher = new BinarySearcher<>(new Integer[] {});
+        assertEquals(-1, searcher.search(1));
+    }
+
+    @Test
+    public void testSingle() {
+        BinarySearcher<Integer> searcher = new BinarySearcher<>(new Integer[] { 1 });
+        assertEquals(-1, searcher.search(-1));
+        assertEquals(0, searcher.search(1));
+        assertEquals(-1, searcher.search(2));
+    }
+
+    @Test
+    public void testDouble() {
+        BinarySearcher<Integer> searcher = new BinarySearcher<>(new Integer[] { 1, 2 });
+        assertEquals(-1, searcher.search(0));
+        assertEquals(0, searcher.search(1));
+        assertEquals(1, searcher.search(2));
+        assertEquals(-1, searcher.search(3));
+    }
+
+    @Test
+    public void testTriple() {
+        BinarySearcher<Integer> searcher = new BinarySearcher<>(new Integer[] { 1, 2, 3 });
+        assertEquals(-1, searcher.search(0));
+        assertEquals(0, searcher.search(1));
+        assertEquals(1, searcher.search(2));
+        assertEquals(2, searcher.search(3));
+        assertEquals(-1, searcher.search(4));
+    }
+
+    @Test
+    public void testBounds() {
+        BinarySearcher<Integer> searcher = new BinarySearcher<>(new Integer[] { 0, 1, 5, 5, 5, 5, 6, 7, 8, 9 });
+        assertNull(searcher.searchBounds(-1));
+        assertArrayEquals(new int[] { 0, 0 }, searcher.searchBounds(0));
+        assertArrayEquals(new int[] { 2, 5 }, searcher.searchBounds(5));
+        assertArrayEquals(new int[] { 8, 8 }, searcher.searchBounds(8));
+        assertArrayEquals(new int[] { 9, 9 }, searcher.searchBounds(9));
+        assertNull(searcher.searchBounds(10));
+    }
+
+}
